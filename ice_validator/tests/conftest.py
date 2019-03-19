19aebe9f1ce92203df5383dbaf1edcc2662d28e4
Bozawglanian, Hagop (hb755d) <hb755d@att.com>
2019-03-19 18:25:52 +0000

    VVP - Fixing script to include static validations
    Change-Id: I0b210d788439e856c8229430abf7264768843867
    Issue-ID: VVP-181
    Signed-off-by: Bozawglanian, Hagop (hb755d) <hb755d@att.com>

--- ice_validator/tests/conftest.py
+++ ice_validator/tests/conftest.py
@@ -1043,9 +1043,7 @@ def build_rst_json(reqs):
     for key, values in list(data.items()):
         if "Heat" in (values["docname"]):
             if "MUST" in (values["keyword"]):
-                if "none" in (values["validation_mode"]):
-                    del data[key]
-                else:
+                if "static" in (values["validation_mode"]):
                     # Creates links in RST format to requirements and test cases
                     if values["test_case"]:
                         mod = values["test_case"].split(".")[-1]
@@ -1054,7 +1052,10 @@ def build_rst_json(reqs):
                         title = "`" + values["id"] + " <" + VNFRQTS_ID_URL + values["docname"].replace(" ", "%20") + ".html#" + values["id"] + ">`_"
                         data[key].update({'full_title': title, 'test_case': rst_value})
                     else:
-                        del data[key]
+                        title = "`" + values["id"] + " <" + VNFRQTS_ID_URL + values["docname"].replace(" ", "%20") + ".html#" + values["id"] + ">`_"
+                        data[key].update({'full_title': title, 'test_case': "No test for requirement", 'validated_by': "static"})
+                else:
+                    del data[key]
             else:
                 del data[key]
         else:
